e375bba94c594b66003993b93e9fcb1e655d5d20
Samtav007 <samtav0821@gmail.com>
2024-05-22 14:28:25 +0530

    done2

--- Client/asap/src/Components/Login.jsx
+++ Client/asap/src/Components/Login.jsx
@@ -96,6 +96,7 @@ const Register = () => {
 
 const Logout = () => {
   const navigate = useNavigate();
+  document.cookie = 'token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;';
 
   React.useEffect(() => {
     setTimeout(() => {
